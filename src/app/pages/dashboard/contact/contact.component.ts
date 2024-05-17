d931be24ee9c3efc7fc58e6cd484607a4cb5e2b0
Mihir Joshi <jmihir2011@gmail.com>
2024-05-17 16:02:13 +0530

    commit changes

--- src/app/pages/dashboard/contact/contact.component.ts
+++ src/app/pages/dashboard/contact/contact.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-  name = window.localStorage.getItem('angularName')
+  name = window.localStorage.getItem('angularName');
+  email: string = 'jmihir2011@gmail.com';
+  mobile: string = '7984304498';
+  linkedin: string = 'https://linkedin.com/in/mj1605'
 }
